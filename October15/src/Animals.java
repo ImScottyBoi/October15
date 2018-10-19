bc909afc1712abfb356a581476a05f6bbdc95779
scott <scott@10.17.210.255>
2018-10-19 15:50:18 -0600

    Added Comments

--- October15/src/Animals.java
+++ October15/src/Animals.java
@@ -1,18 +1,24 @@
+/*
+ * Scott Weeden
+ * Date: 10.17.18
+ * Purpose: Allow for people to get random score's for animals.
+ */
 
+//attributes
 public class Animals {
 	private String height;
 	private String legs;
 	private String weight;
 	private String name;
 	private double random;
-	
+	//constructor
 	public Animals(String name, String height, String legs, String weight) {
 		this.height = height;
 		this.legs = legs;
 		this.weight = weight;
 		this.name = name;
 	}
-	
+	//score method
 	public void score() {
 		random = Math. random() * 9 + 1;
 	}
